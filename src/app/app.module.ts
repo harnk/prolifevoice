d9b9f2f3b5dc6ed029681793a5d361ee11aa3372
Scott Null <scott.null@gmail.com>
2020-05-27 22:23:50 -0400

    add bottombar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottombarComponent } from './bottombar/bottombar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottombarComponent
   ],
   imports: [
     BrowserModule,
