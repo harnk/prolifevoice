452006aa5d3f5481ca9bde827853d7544ff3c600
Scott Null <scott.null@gmail.com>
2020-07-02 23:22:13 -0400

    cleanup WIP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,9 @@ import { IQuote } from '../quote';
 export class HomeComponent implements OnInit {
 
   public today = Date.now();
-  public id = "";
   public dayadjust = 0;
   public items: Array<IQuote> = [];
   public activeItem: IQuote;
-  public dayquote = "";
-  public authorquote = ""
   public datestr = "";
   private homeIndex: number;
 
@@ -25,52 +22,31 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("HomeComponent init");
-
-    this._Activatedroute.paramMap.subscribe(params => { 
-      this.id = params.get('id'); 
-    });
-
-    console.log("passed in id = " + this.id);
-
-    // work on this https://www.tektutorialshub.com/angular/angular-passing-parameters-to-route/
-
-    // console.log(params.get('id'));
     this._quoteService.getQuotes().subscribe(quoteList => this.items = quoteList);
     this._quoteService.getQuoteOfTheDay().subscribe((qotd) => {
-      // add a check for qotd[].post_date == today then return that quote
       var q = new Date();
-
       var tday = new Date(q.getFullYear(),q.getMonth(),q.getDate());
-      var x = this.id;
-      var y: number = +x;
-      this.dayadjust = this.dayadjust + y;
-      console.log("dayadjust = " + this.dayadjust);
-      var datestr = this.formatDate(tday, y);
+      var datestr = this.formatDate(tday);
       console.log('datestr: '+datestr);
-      this.setQuoteAndAuthor(qotd, datestr);
-       
+      this.setActiveItemForToday(qotd, datestr);
     });
   }
 
-  private setQuoteAndAuthor(qotd: IQuote[], datestr: string) {      
-    // ?? need to set some default quote if the date isnt found
+  private setActiveItemForToday(qotd: IQuote[], datestr: string) {      
+    this.homeIndex = 1; // default of date not found
     for (var i = 0; i < qotd.length; i++) {
       if (datestr === qotd[i].post_date) {
         console.log('FOUND IT ' + datestr);
-        // Set the quote of the day
-        this.dayquote = qotd[i].quote;
-        console.log('this.dayquote: ' + this.dayquote);
-        this.authorquote = qotd[i].author;
         this.activeItem = qotd[i];
         this.homeIndex = i;
       }
     }
   }
 
-  private formatDate(date, adjust) {
+  private formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
-        day = '' + (d.getDate() + adjust),
+        day = '' + d.getDate(),
         year = d.getFullYear();
 
     if (month.length < 2) 
