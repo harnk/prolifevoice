97fdaaaafaefb6e856db2d99c179dc6014055aad
Scott Null <scott.null@gmail.com>
2020-07-13 22:16:45 -0400

    cleanup WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule, ActivatedRoute, ParamMap } from '@angular/router'
 
 import { HomeComponent } from './home/home.component';
 import { MoreComponent } from './more/more.component';
-import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShareComponent } from './share/share.component';
 
@@ -13,7 +12,6 @@ const routes: Routes = [
   { path: 'home/:id', component: HomeComponent },
   { path: 'more', component: MoreComponent },
   { path: 'more/:id', component: MoreComponent },
-  { path: 'search', component: SearchComponent },
   { path: 'search-results', component: SearchResultsComponent },
   { path: 'share', component: ShareComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
